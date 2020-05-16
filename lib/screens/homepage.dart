c92b341bfaf795a30a6ec41b588274acfe1f2474
Kiran Randhawa <Kiran.Randhawa85@gmail.com>
2020-05-16 10:40:13 +0100

    Theme and logo

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -44,7 +44,9 @@ class _HomepageState extends State<Homepage> {
       appBar: AppBar(
         // Here we take the value from the Homepage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+
+        title: Image.asset('/assets/Revamp.png', fit: BoxFit.cover),
+        //title: Text(widget.title),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
